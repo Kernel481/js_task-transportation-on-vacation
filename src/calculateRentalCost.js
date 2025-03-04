646b19cd112ad17712bf3cf5dc73186f7b63c750
Kernel481 <kernel481@gmail.com>
2025-03-04 13:58:38 +0200

    Task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseprice = 40;
+  const basicTerm = 3;
+  const basicDiscount = 20;
+  const longTerm = 7;
+  const longDiscount = 50;
+
+  if (days < basicTerm) {
+    return days * baseprice;
+  }
+
+  if (days < longTerm) {
+    return days * baseprice - basicDiscount;
+  }
+
+  if (days >= longTerm) {
+    return days * baseprice - longDiscount;
+  }
 }
 
 module.exports = calculateRentalCost;
